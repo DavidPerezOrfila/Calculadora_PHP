6d9271414b89344a57c68003a8507c858818c051
DavidPerezOrfila <knightbcn@gmail.com>
2016-10-07 17:58:53 +0200

    do_operacion

---
+++ M7 PHP/Calculadora/do_operacion.php
@@ -0,0 +1,15 @@
+<html xmlns="http://www.w3.org/1999/html">
+<head>
+    <title>Solucion</title>
+</head>
+<body>
+<div id="header">
+    <ul>
+        <li><a href="calculadoraHtml.html">Calculadora</a></li>
+    </ul>
+
+<div id="suma">
+    <h2>La suma de los dos números es: <?php echo $_POST['primerOperador'] + $_POST['segundoOperador'];?></h2>
+</div>
+</body>
+</html>
